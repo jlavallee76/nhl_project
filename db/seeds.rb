53719ccaddb324d094a9755ac638de50991e25c0
Josh Lavallee <jlavallee76@academic.rrc.ca>
2021-02-17 10:23:48 -0600

    Seed file retrieves team information from NHL API

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "open-uri"
+require "json"
+
+# Fetches and decodes all team data from the NHL stats API
+def team_fetch(url)
+  JSON.parse(open(url).read)
+end
+
+# Gets specific team data based on the team id passed
+def team_info(id)
+  team_fetch("https://statsapi.web.nhl.com/api/v1/teams/#{id}")
+end
+
+team_ids = 1..30
+team_ids.each do |team_id|
+  team = team_info(team_id)
+  puts team["teams"][0]["name"]
+  puts team["teams"][0]["venue"]["name"]
+  puts team["teams"][0]["firstYearOfPlay"]
+  puts team["teams"][0]["conference"]["name"]
+  puts team["teams"][0]["division"]["name"]
+  puts team["teams"][0]["officialSiteUrl"]
+end
